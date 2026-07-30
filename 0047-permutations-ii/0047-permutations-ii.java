dbfe0ed8d998fdc58271fa23f799b451fe436dc6
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-07-30 12:27:00 +0530

    Time: 708 ms (5.09%), Space: 47.3 MB (6.15%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,30 +1,45 @@
+import java.util.*;
+
 class Solution {
-    public void permutation(int idx, int[]nums,List<List<Integer>>ans){
-        if(idx==nums.length){
-            List<Integer>ds=new ArrayList<>();
-            for(int i=0;i<nums.length;i++){
+    public void helper(int index, int[] nums, List<List<Integer>> ans) {
+        // Base Case: Raasta khatam, poora permutation ban gaya
+        if (index == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
                 ds.add(nums[i]);
             }
-            ans.add(ds);
+            
+            // 🚀 TERA IDEA: Yahan hum check kar rahe hain
+            // Agar yeh list pehle se ans mein nahi hai, tabhi add karo
+            if (!ans.contains(ds)) {
+                ans.add(ds);
+            }
             return;
         }
-        HashSet<Integer>set=new HashSet<>();
-        for(int i=idx;i<nums.length;i++){
-            if(set.contains(nums[i]))continue;
-            set.add(nums[i]);
-            swap(i,idx,nums);
-            permutation(idx+1,nums,ans);
-            swap(idx,i,nums);// backtracking
+        
+        // Loop chalega, par ab koi rokne wala (HashSet/Condition) nahi hai
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            
+            // Explore
+            helper(index + 1, nums, ans);
+            
+            // Backtrack
+            swap(i, index, nums);
         }
     }
-    public void swap(int i,int j,int[]nums){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
+    
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>>ans=new ArrayList<>();
-        permutation(0,nums,ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        // Is approach mein array ko sort karne ki bhi zaroorat nahi hai, 
+        // kyunki .contains() khud hi check kar lega!
+        helper(0, nums, ans);
         return ans;
     }
 }
