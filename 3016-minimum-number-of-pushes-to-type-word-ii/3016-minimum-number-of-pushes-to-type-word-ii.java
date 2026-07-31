067702e69b20605f825bad831ec9717dec9fa078
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-07-31 16:03:23 +0530

    Time: 9 ms (94.22%), Space: 48 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int minimumPushes(String word) {
-        int map[]=new int[26];
-        for(int i=0;i<word.length();i++){
-            map[word.charAt(i)-'a']++;
+        int[] map = new int[26];
+        for (char c : word.toCharArray()) {
+            map[c - 'a']++;
         }
         Arrays.sort(map);
-        int count=0;
-        int j=0;
-        for(int i=25;i>=0;i--){
-            if(map[i]==0)break;
-            count=count+(j/8+1)*map[i];
+        int count = 0;
+        int j = 0;   
+        for (int i = 25; i >= 0; i--) {
+            if (map[i] == 0) break;
+            count += ((j >> 3) + 1) * map[i];
             j++;
-
         }
         return count;
     }
