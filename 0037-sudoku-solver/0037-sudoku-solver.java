ebe16acef0a90d42bd1fb6ee4fd9710b69146db5
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-01 16:03:45 +0530

    Time: 173 ms (81.11%), Space: 43.1 MB (8.25%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isSafe(char[][]board,int row, int col,char digit){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==digit)return false;
+        }
+        for(int i=0;i<9;i++){
+            if(board[i][col]==digit)return false;
+        }
+        int sr=(row/3)*3;
+        int sc=(col/3)*3;
+        for(int i=sr;i<sr+3;i++){
+            for(int j=sc;j<sc+3;j++){
+                if(board[i][j]==digit)return false;
+            }
+        }
+        return true;
+    }
+    public boolean solver(char[][]board,int row,int col){
+        if(row==board.length){
+            return true;
+        }
+        int nextrow=row;
+        int nextcol=col+1;
+        if(nextcol==9){
+            nextrow+=1;
+            nextcol=0;
+        }
+        if(board[row][col]!='.'){
+            return solver(board,nextrow,nextcol);
+        }
+        for(char digit='1';digit<='9';digit++){
+            if(isSafe(board,row,col,(char)digit)){
+                board[row][col]=digit;
+                if(solver(board,nextrow,nextcol)){
+                    return true;
+                }
+                board[row][col]='.';
+            }
+        }
+        return false;
+    }
+    public void solveSudoku(char[][] board) {
+        solver(board,0,0);
+    }
+}
