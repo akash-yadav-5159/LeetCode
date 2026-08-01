877da293e1734b75019b8fa0d7334abc3946a7c0
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-01 11:28:14 +0530

    Time: 7 ms (43.17%), Space: 46.5 MB (99.02%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,23 +1,18 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        List<List<Integer>>ans=new ArrayList<>();
-        int m=grid.length;
-        int n=grid[0].length;
-        int len=k%(m*n);
-        int row[]=new int[m*n];
-        int l=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                row[l]=grid[i][j];
-                l++;
-            }
-        }
-        if(len==0)len=m*n;
-        int x=m*n-len;
-        List<Integer>temp=new ArrayList<>();
-        for(int i=x;i<row.length+x;i++){
-            temp.add(row[i%row.length]);
-            if(temp.size()==n){
+        List<List<Integer>> ans = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        int totalElements = m * n;
+        k = k % totalElements;
+        int start = (totalElements - k) % totalElements; 
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < totalElements; i++) {
+            int index = (start + i) % totalElements;
+            int r = index / n;
+            int c = index % n;
+            temp.add(grid[r][c]); 
+            if (temp.size() == n) {
                 ans.add(new ArrayList<>(temp));
                 temp.clear();
             }
