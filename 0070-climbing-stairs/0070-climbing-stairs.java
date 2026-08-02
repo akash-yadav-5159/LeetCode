3f138a7b9164876adea36c3c6631e9934d578e3a
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-02 12:04:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,12 @@
 class Solution {
-    public int ans(int[]dp, int n){
-        if(dp[n]!=-1)return dp[n];
-        if(n==2||n==1){
-            return dp[n]=n;
-        }
-        int single=ans(dp,n-1);
-        int doubles=ans(dp,n-2);
-        dp[n]=single+doubles;
-        return dp[n];
-    }
     public int climbStairs(int n) {
+        if(n==1||n==2)return n;
         int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return ans(dp,n);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
