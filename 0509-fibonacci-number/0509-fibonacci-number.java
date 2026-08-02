ac0d12d17d8be29062e13da2b546a9c79c9466c9
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-02 09:47:18 +0530

    Time: 20 ms (9.32%), Space: 42.3 MB (16.12%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int recu(int n,int []dp){
+        if(n==0||n==1){
+            dp[n]=n;
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return (recu(n-1,dp)+recu(n-2,dp));
+    }
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return recu(n,dp);
+    }
+}
