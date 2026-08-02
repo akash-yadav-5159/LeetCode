ef581043c98b074aa947c1b940b3c211d0f491ae
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-02 10:29:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,24 @@
 class Solution {
-    public int recu(int n,int []dp){
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        if(n==0||n==1){
-            dp[n]=n;
+    public int fib(int n) {
+        // Agar n 0 ya 1 hai, toh seedha n return kar do agar ye base condition nhi likhe to n=0 k liye out of bound dega, kyuki hum dp[1] ko access hi nhi kar paayenge
+        if (n == 0 || n == 1) {
             return n;
         }
-        return dp[n]=(recu(n-1,dp)+recu(n-2,dp));
-    }
-    public int fib(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return recu(n,dp);
+        
+        // 1. DP array banayenge (size n+1 taaki n index exist kare)
+        int[] dp = new int[n + 1];
+        
+        // 2. Base cases ko array mein manually daalenge
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        // 3. Bottom-Up calculation: Loop 2 se shuru hoga n tak jayega
+        for (int i = 2; i <= n; i++) {
+            // Har naya answer pichle do answers ka sum hoga
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        // 4. Loop khatam hone ke baad final answer dp[n] mein hoga
+        return dp[n];
     }
 }
