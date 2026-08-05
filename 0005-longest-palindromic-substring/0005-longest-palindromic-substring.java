2ddfff66b765c81bf2ff5000e0461a2e373c9851
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-05 15:09:38 +0530

    Time: 306 ms (18.86%), Space: 175.7 MB (5.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,24 +1,35 @@
 class Solution {
+    Integer[][] memo; 
     public String longestPalindrome(String s) {
         int n = s.length();
-        for (int windowSize = n; windowSize > 0; windowSize--) {
-            for (int start = 0; start <= n - windowSize; start++) {
-                int end = start + windowSize - 1;
-                if (isPalindrome(s, start, end)) {
-                    return s.substring(start, end + 1);
+        memo = new Integer[n][n]; 
+        int start = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkPalindrome(s, i, j)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
                 }
             }
         }
-        return "";
+        return s.substring(start, start + maxLen);
     }
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+    private boolean checkPalindrome(String s, int i, int j) {
+        if (i >= j) {
+            return true;
         }
-        return true;
+        if (memo[i][j] != null) {
+            return memo[i][j] == 1;
+        }
+        boolean isPalin = false;
+        if (s.charAt(i) == s.charAt(j)) {
+            isPalin = checkPalindrome(s, i + 1, j - 1);
+        }
+        memo[i][j] = isPalin ? 1 : 2;
+        
+        return isPalin;
     }
 }
