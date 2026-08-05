2d2064514ca2622fb9984f27e28bf07a047e2fd4
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-05 15:50:43 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,17 @@
 class Solution {
+    public int ans(int [][]dp,int m,int n,int cr,int cc){
+        if(dp[cr][cc]!=-1)return dp[cr][cc];
+        if(cr==m||cc==n)return 0;
+        if(cr==m-1&&cc==n-1)return 1;
+        int firstright=ans(dp,m,n,cr,cc+1);
+        int firstdown=ans(dp,m,n,cr+1,cc);
+        return dp[cr][cc]=firstright+firstdown;
+    }
     public int uniquePaths(int m, int n) {
-        // N = Total steps, R = Steps to go right (ya down)
-        int N = m + n - 2;
-        int R = Math.min(m - 1, n - 1); // Chhoti value lene se loop kam chalega
-        
-        long ans = 1; // long use kar rahe hain taaki intermediate calculation overflow na ho
-        
-        for (int i = 1; i <= R; i++) {
-            ans = ans * (N - R + i) / i;
+        int dp[][]=new int[m+1][n+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
         }
-        
-        return (int) ans;
+        return ans(dp,m,n,0,0);
     }
 }
