3368f78c913326ff944621f4273da72ef8363490
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-07 15:30:51 +0530

    Time: 84 ms (29.28%), Space: 74.8 MB (21.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean ans(int []nums,  Boolean dp[][],int sum, int idx){
+        if(sum==0)return true;
         if(idx>=nums.length||sum<0)return false;
         if(dp[idx][sum]!=null)return dp[idx][sum];
-        if(sum==0)return true;
         boolean include=ans(nums,dp,sum-nums[idx],idx+1);
         boolean exclude=ans(nums,dp,sum,idx+1);
         return dp[idx][sum]=include||exclude;
