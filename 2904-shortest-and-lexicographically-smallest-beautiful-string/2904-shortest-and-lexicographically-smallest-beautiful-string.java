d8107c8e31a35b056ae947e880b527be826d3b12
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-08-26 11:44:06 +0530

    Time: 1 ms (100%), Space: 43.5 MB (99.33%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,46 +1,34 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        StringBuilder finals=new StringBuilder();
-        int count=0;
-        int start=0;
-        int fixstart=0;
-        int fixend=0;
-        boolean x=true;
-        int len=s.length()+1;
-        for(int j=0;j<s.length();j++){
-            if(s.charAt(j)=='1')count++;
-            while(start <= j && s.charAt(start) == '0') {
-                start++;
+        int left = 0;
+        int onesCount = 0;
+        String result = "";
+        int minLen = Integer.MAX_VALUE;
+        for (int right = 0; right < s.length(); right++) {
+            if (s.charAt(right) == '1') {
+                onesCount++;
             }
-            if(count==k){
-                x=false;
-                if((j-start+1)<len){
-                    len=j-start+1;
-                    fixstart=start;
-                    fixend=j;
-                } else if (j-start+1 == len) {
-                    String currentStr = s.substring(start, j + 1);
-                    String bestStr = s.substring(fixstart, fixend + 1);
-                    if(currentStr.compareTo(bestStr) < 0) {
-                        fixstart = start;
-                        fixend = j;
+            while (onesCount == k) {
+                while (left < right && s.charAt(left) == '0') {
+                    left++;
+                }
+                int currentLen = right - left + 1;
+                String sub = s.substring(left, right + 1);
+                if (currentLen < minLen) {
+                    minLen = currentLen;
+                    result = sub;
+                }
+                else if (currentLen == minLen) {
+                    if (sub.compareTo(result) < 0) {
+                        result = sub;
                     }
                 }
-                int subcount=0;
-                for(int i=start;i<=j;i++){
-                    if(s.charAt(i)=='1')subcount++;
-                    if(subcount==2){
-                        start=i;
-                        break;
-                    }
+                if (s.charAt(left) == '1') {
+                    onesCount--;
                 }
-                count=k-1;
+                left++;
             }
         }
-        if(x)return "";
-        for(int i=fixstart;i<=fixend;i++){
-            finals.append(s.charAt(i));
-        }
-        return finals.toString();
+        return result;
     }
 }
