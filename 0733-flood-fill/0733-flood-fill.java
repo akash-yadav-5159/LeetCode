26105af1357bc6a31d1445d58daada8e45f9dd65
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-09-15 15:41:54 +0530

    Time: 0 ms (100%), Space: 46.7 MB (61.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,31 +1,23 @@
 class Solution {
-    public void dfs(int [][]image, boolean vis[][], int sr, int sc,int comp){
-        vis[sr][sc]=true;
-        if(sr!=0&&!vis[sr-1][sc]&&image[sr-1][sc]==comp){
-            dfs(image,vis,sr-1,sc,comp);
+    public void dfs(int [][]image, int sr, int sc,int comp,int color){
+        image[sr][sc]=color;
+        if(sr!=0&&image[sr-1][sc]==comp){
+            dfs(image,sr-1,sc,comp,color);
         }
-        if(sr!=image.length-1&&!vis[sr+1][sc]&&image[sr+1][sc]==comp){
-            dfs(image,vis,sr+1,sc,comp);
+        if(sr!=image.length-1&&image[sr+1][sc]==comp){
+            dfs(image,sr+1,sc,comp,color);
         }
-        if(sc!=0&&!vis[sr][sc-1]&&image[sr][sc-1]==comp){
-            dfs(image,vis,sr,sc-1,comp);
+        if(sc!=0&&image[sr][sc-1]==comp){
+            dfs(image,sr,sc-1,comp,color);
         }
-        if(sc!=image[0].length-1&&!vis[sr][sc+1]&&image[sr][sc+1]==comp){
-            dfs(image,vis,sr,sc+1,comp);
+        if(sc!=image[0].length-1&&image[sr][sc+1]==comp){
+            dfs(image,sr,sc+1,comp,color);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean vis[][]=new boolean[image.length][image[0].length];
         int comp=image[sr][sc];
         if(comp==color)return image;
-        dfs(image,vis,sr,sc,comp);
-        for(int i=0;i<image.length;i++){
-            for(int j=0;j<image[0].length;j++){
-                if(vis[i][j]){
-                    image[i][j]=color;
-                }
-            }
-        }
+        dfs(image,sr,sc,comp,color);
         return image;
     }
 }
