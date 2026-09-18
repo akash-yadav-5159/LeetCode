8e0076506a44977b283be59f8ffbceebf3158432
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2026-09-18 13:24:17 +0530

    Time: 1 ms (100%), Space: 47.6 MB (7.3%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -1,30 +1,34 @@
 class Solution {
-    public void reverse(int arr[],int i, int j){
-        while(i<j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+    public void reverseRow(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public void reverseColumn(int[][] grid, int col, int i, int j) {
+        while (i < j) {
+            int temp = grid[i][col];
+            grid[i][col] = grid[j][col];
+            grid[j][col] = temp;
             i++;
             j--;
         }
     }
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
-        for(int i=0;i<n;i++){
-            reverse(grid[i],0,rowShift[i]-1);
-            reverse(grid[i],rowShift[i],n-1);
-            reverse(grid[i],0,n-1);
+        for (int i = 0; i < n; i++) {
+            int k = rowShift[i] % n;
+            reverseRow(grid[i], 0, k - 1);
+            reverseRow(grid[i], k, n - 1);
+            reverseRow(grid[i], 0, n - 1);
         }
-        for(int i=0;i<n;i++){
-            int nums[]=new int[n];
-            for(int j=0;j<n;j++){
-                nums[j]=grid[j][i];
-            }
-            reverse(nums,0,colShift[i]-1);
-            reverse(nums,colShift[i],n-1);
-            reverse(nums,0,n-1);
-            for(int j=0;j<n;j++){
-                grid[j][i]=nums[j];
-            }
+        for (int col = 0; col < n; col++) {
+            int k = colShift[col] % n;
+            reverseColumn(grid, col, 0, k - 1);
+            reverseColumn(grid, col, k, n - 1);
+            reverseColumn(grid, col, 0, n - 1);
         }
         return grid;
     }
